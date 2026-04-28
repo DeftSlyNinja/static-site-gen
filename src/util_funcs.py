f1b7f098396bd2539b780815afb879eb99a87509
DeftSlyNinja <christopher_masson@hotmail.com>
2026-04-28 09:58:48 -0700

    Added markdown to blocks function and tests

--- src/util_funcs.py
+++ src/util_funcs.py
@@ -7,3 +7,14 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def markdown_to_blocks(markdown: str):
+    if markdown == "":
+        return [markdown]
+    blocks = markdown.split('\n\n')
+    final_blocks = []
+    for block in blocks:
+        block = block.strip()
+        if block != "":
+            final_blocks.append(block)
+    return final_blocks
